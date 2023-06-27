676aae5b4ad4dfe99c685eef2c39d32989bada53
NatLittleton <n.littletonrua@gmail.com>
2023-06-27 15:02:36 -0300

    projeto-final

---
+++ exercicio 13-06-22/app.js
@@ -0,0 +1,20 @@
+// const nome = "Natasha"
+// const sobrenome = "Littleton"
+
+// console.log(nome);
+//console.log(sobrenome);
+//console.log(nome, sobrenome)
+
+//const number = ["1", "2", "3", "4", "5", "6", "7"]
+//console.log(1, 3, 7)
+//console.log(number[0])
+//console.log(number[2])
+// console.log(number[6])
+
+const lista = ["arroz", "macarrão", "3 litros de sabão", "natasha", "anubinho", "o mais lindo"]
+console.log(lista[0])
+console.log(lista[3])
+console.log(lista[lista.length -1])
+
+const botao = document.getElementById
+console.log(botao)
---
+++ projeto-final/app.js
@@ -0,0 +1,15 @@
+const btnMobile = document.getElementById ('Logo');
+
+function toggleMenu() {
+    const nav = document.getElementById ('nav');
+    nav.classList.toggle('active')
+}
+
+const botao = document.getElementById
+console.log(botao)
+
+function addStyle() {
+    const sheet = new CSSStyleSheet ();
+    sheet.insertRule ('button{background: purple; 
+        ')
+}
